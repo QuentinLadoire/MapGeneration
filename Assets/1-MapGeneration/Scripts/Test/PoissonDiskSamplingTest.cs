5ef5142e9037e552d7e86ee6bd9b5916deef45db
Quentin Ladoire <quentinladoire@gmail.com>
2021-10-18 19:54:08 +0200

    Add a option to select the StartPoint pick mode
    Mode :
        - Random : random in the area
        - Center : center of the area
        - TopLeft : top left of the area
        - TopRight : top right of the area
        - BottomLeft : bottom left of the area
        - BottomRight : bottom right of the area
        - Custom : specified point

--- Assets/1-MapGeneration/Scripts/Test/PoissonDiskSamplingTest.cs
+++ Assets/1-MapGeneration/Scripts/Test/PoissonDiskSamplingTest.cs
@@ -1,43 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PoissonDiskSamplingTest : MonoBehaviour
+namespace PoissonDisk.Unity
 {
-	[Header("PoissonDiskSampling Settings")]
-	[SerializeField] private float radius = 1.0f;
-	[SerializeField] private float areaWidth = 30.0f;
-	[SerializeField] private float areaHeight = 30.0f;
-	[SerializeField] private int sampleLimitBeforeRejection = 30;
-	[SerializeField] private int seed = 0;
-
-	Point2D[] points = null;
-	PoissonDiskSampling poissonDiskSampling = null;
-
-	private void GenerateTexture()
+	public class PoissonDiskSamplingTest : MonoBehaviour
 	{
-		var texture = new Texture2D(256, 256);
-		texture.filterMode = FilterMode.Point;
-		foreach (var point in points)
-			texture.SetPixel((int)(256 * (point.x / areaWidth)), (int)(256 * (point.y / areaHeight)), Color.red);
+		[Header("PoissonDiskSampling Settings")]
+		[SerializeField] private float radius = 1.0f;
+		[SerializeField] private float areaWidth = 30.0f;
+		[SerializeField] private float areaHeight = 30.0f;
+		[SerializeField] private int sampleLimitBeforeRejection = 30;
+		[SerializeField] private int seed = 0;
+		[SerializeField] private StartPointPickMode startPointPickMode = StartPointPickMode.Random;
+		[SerializeField] private Vector2 customStartPoint = Vector2.zero;
 
-		texture.Apply();
+		Point2D[] points = null;
+		PoissonDiskSampling poissonDiskSampling = null;
+		
+		private void GenerateTexture()
+		{
+			var texture = new Texture2D(256, 256);
+			texture.filterMode = FilterMode.Point;
+			foreach (var point in points)
+				texture.SetPixel((int)((256 - 1) * (1 - (point.x / areaWidth))), (int)((256 - 1) * (1 - (point.y / areaHeight))), Color.red);
 
-		GetComponent<MeshRenderer>().sharedMaterial.mainTexture = texture;
-	}
+			texture.Apply();
 
-	private void OnValidate()
-	{
-		if (poissonDiskSampling == null)
-			poissonDiskSampling = new PoissonDiskSampling();
+			GetComponent<MeshRenderer>().sharedMaterial.mainTexture = texture;
+		}
+		private void Generate()
+		{
+			if (poissonDiskSampling == null)
+				poissonDiskSampling = new PoissonDiskSampling();
 
-		poissonDiskSampling.Radius = radius;
-		poissonDiskSampling.AreaWidth = areaWidth;
-		poissonDiskSampling.AreaHeight = areaHeight;
-		poissonDiskSampling.SampleLimitBeforeRejection = sampleLimitBeforeRejection;
-		poissonDiskSampling.Seed = seed;
+			poissonDiskSampling.Radius = radius;
+			poissonDiskSampling.AreaWidth = areaWidth;
+			poissonDiskSampling.AreaHeight = areaHeight;
+			poissonDiskSampling.SampleLimitBeforeRejection = sampleLimitBeforeRejection;
+			poissonDiskSampling.Seed = seed;
+			poissonDiskSampling.StartPointPickMode = startPointPickMode;
+			poissonDiskSampling.CustomStartPoint = new Point2D(customStartPoint.x, customStartPoint.y);
 
-		poissonDiskSampling.GeneratePoints(ref points);
-		GenerateTexture();
+			poissonDiskSampling.GeneratePoints(ref points);
+			GenerateTexture();
+		}
+
+		private void OnValidate()
+		{
+			Generate();
+		}
 	}
 }
--- Assets/9-Extern/PoissonDiskSampling/PoissonDiskSampling.cs
+++ Assets/9-Extern/PoissonDiskSampling/PoissonDiskSampling.cs
@@ -1,171 +1,223 @@
 using System;
 using System.Collections.Generic;
 
-public struct Point2D
+namespace PoissonDisk
 {
-    public float x;
-    public float y;
+    public struct Point2D
+    {
+        public float x;
+        public float y;
 
-    public Point2D(float x, float y)
-	{
-        this.x = x;
-        this.y = y;
-	}
-}
-
-public class PoissonDiskSampling
-{
-    private const float Pi2 = 6.28318530718f;
-    private const float Sqrt2 = 1.41421356237f;
-
-    private int gridWidth = 0;
-    private int gridHeight = 0;
-    private float cellSize = 0.0f;
-
-    private int seed = 0;
-    private Random random = new Random(0);
-
-    private int[,] grid = null;
-    private List<int> processingPointList = new List<int>();
-    private List<Point2D> outputPointList = new List<Point2D>();
-
-    public float Radius { get; set; } = 1.0f;
-    public float AreaWidth { get; set; } = 30.0f;
-    public float AreaHeight { get; set; } = 30.0f;
-    public int SampleLimitBeforeRejection { get; set; } = 30;
-    public int Seed { get => seed; set => SetSeed(value); }
-
-    private int CeilToInt(float value)
-	{
-        return (int)Math.Ceiling(value);
-	}
-    private int FloorToInt(float value)
-	{
-        return (int)Math.Floor(value);
-	}
-    private float Lerp(float start, float end, float t)
-	{
-        return start + ((end - start) * t);
-	}
-    private float SqrtDistance(Point2D p1, Point2D p2)
-	{
-        var x = p2.x - p1.x;
-        var y = p2.y - p1.y;
-
-        return x * x + y * y;
-	}
-
-    private bool IsValidPointPosition(Point2D point)
-	{
-        //Check if point is in area
-        if (point.x < 0.0f || point.x > AreaWidth ||
-            point.y < 0.0f || point.y > AreaHeight)
-            return false;
-
-        //Get only nearby cells
-        var gridX = FloorToInt(point.x / cellSize);
-        var gridY = FloorToInt(point.y / cellSize);
-        var startX = Math.Max(0, gridX - 2);
-        var startY = Math.Max(0, gridY - 2);
-        var endX = Math.Min(gridWidth - 1, gridX + 2);
-        var endY = Math.Min(gridHeight - 1, gridY + 2);
-
-        //Check if point is within distance r of existing samples
-        for (int j = startY; j <= endY; j++)
-            for (int i = startX; i <= endX; i++)
-            {
-                var pointIndex = grid[i, j];
-                if (pointIndex != -1)
-                {
-                    var pointToTest = outputPointList[pointIndex];
-
-                    var sqrtDistance = SqrtDistance(pointToTest, point);
-                    if (sqrtDistance < Radius * Radius)
-                        return false;
-                }
-            }
-
-        return true;
-	}
-
-    public void SetSeed(int seed)
-	{
-        this.seed = seed;
-        random = new Random(seed);
-	}
-    public void GeneratePoints(ref Point2D[] points)
-	{
-        //Determine Grid Size
-        cellSize = Radius / Sqrt2;
-        gridWidth = CeilToInt(AreaWidth / cellSize);
-        gridHeight = CeilToInt(AreaHeight / cellSize);
-
-        //Initialize Grid and List
-        grid = new int[gridWidth, gridHeight];
-        grid.Fill(-1);
-
-        outputPointList.Clear();
-        processingPointList.Clear();
-
-        //Pick a Random Point
-        var startPoint = new Point2D
+        public Point2D(float x, float y)
         {
-            x = (float)(AreaWidth * random.NextDouble()),
-            y = (float)(AreaHeight * random.NextDouble())
-        };
+            this.x = x;
+            this.y = y;
+        }
+    }
 
-        //Add the start point to the output list, processing list and grid
-        outputPointList.Add(startPoint);
-        processingPointList.Add(0);
-        grid[FloorToInt(startPoint.x / cellSize), FloorToInt(startPoint.y / cellSize)] = 0;
+    public enum StartPointPickMode
+	{
+        Random,
+        Center,
+        TopLeft,
+        TopRight,
+        BottomLeft,
+        BottomRight,
+        Custom
+	}
 
-        //Process
-        while (processingPointList.Count > 0)
-		{
-            //Choose a random index from the processing list
-            var randomProcessingListIndex = random.Next(processingPointList.Count);
-            var outputPointIndex = processingPointList[randomProcessingListIndex];
-            var point = outputPointList[outputPointIndex];
+    public class PoissonDiskSampling
+    {
+        private const float Pi2 = 6.28318530718f;
+        private const float Sqrt2 = 1.41421356237f;
 
-            //Generate sampleLimitBeforeRejection Points
-            var noValidPoint = true;
-            for (int i = 0; i < SampleLimitBeforeRejection; i++)
-			{
-                //Generate random angle and distance
-                var angle = Pi2 * (float)random.NextDouble();
-                var distance = Lerp(Radius, 2 * Radius, (float)random.NextDouble());
+        private int gridWidth = 0;
+        private int gridHeight = 0;
+        private float cellSize = 0.0f;
 
-                //Create new Point
-                var newPoint = new Point2D
+        private int seed = 0;
+        private Random random = new Random(0);
+
+        private int[,] grid = null;
+        private List<int> processingPointList = new List<int>();
+        private List<Point2D> outputPointList = new List<Point2D>();
+
+        public float Radius { get; set; } = 1.0f;
+        public float AreaWidth { get; set; } = 30.0f;
+        public float AreaHeight { get; set; } = 30.0f;
+        public int SampleLimitBeforeRejection { get; set; } = 30;
+        public int Seed { get => seed; set => SetSeed(value); }
+
+        public Point2D CustomStartPoint { get; set; } = new Point2D(0.0f, 0.0f);
+        public StartPointPickMode StartPointPickMode { get; set; } = StartPointPickMode.Random;
+
+        private int CeilToInt(float value)
+        {
+            return (int)Math.Ceiling(value);
+        }
+        private int FloorToInt(float value)
+        {
+            return (int)Math.Floor(value);
+        }
+        private float Lerp(float start, float end, float t)
+        {
+            return start + ((end - start) * t);
+        }
+        private float SqrtDistance(Point2D p1, Point2D p2)
+        {
+            var x = p2.x - p1.x;
+            var y = p2.y - p1.y;
+
+            return x * x + y * y;
+        }
+
+        private bool IsValidPointPosition(Point2D point)
+        {
+            //Check if point is in area
+            if (point.x < 0.0f || point.x > AreaWidth ||
+                point.y < 0.0f || point.y > AreaHeight)
+                return false;
+
+            //Get only nearby cells
+            var gridX = FloorToInt(point.x / cellSize);
+            var gridY = FloorToInt(point.y / cellSize);
+            var startX = Math.Max(0, gridX - 2);
+            var startY = Math.Max(0, gridY - 2);
+            var endX = Math.Min(gridWidth - 1, gridX + 2);
+            var endY = Math.Min(gridHeight - 1, gridY + 2);
+
+            //Check if point is within distance r of existing samples
+            for (int j = startY; j <= endY; j++)
+                for (int i = startX; i <= endX; i++)
                 {
-                    x = point.x + distance * (float)Math.Cos(angle),
-                    y = point.y + distance * (float)Math.Sin(angle)
-                };
+                    var pointIndex = grid[i, j];
+                    if (pointIndex != -1)
+                    {
+                        var pointToTest = outputPointList[pointIndex];
 
-                //Check if it is within distance r of existing points
-                if (IsValidPointPosition(newPoint))
-				{
-                    //Add the new point to the output list, processing list and grid
-                    var gridX = FloorToInt(newPoint.x / cellSize);
-                    var gridY = FloorToInt(newPoint.y / cellSize);
-
-                    outputPointList.Add(newPoint);
-                    grid[gridX, gridY] = outputPointList.Count - 1;
-                    processingPointList.Add(outputPointList.Count - 1);
-
-                    noValidPoint = false;
+                        var sqrtDistance = SqrtDistance(pointToTest, point);
+                        if (sqrtDistance < Radius * Radius)
+                            return false;
+                    }
                 }
-			}
 
-            //If no such point is found, remove point from the processing list
-            if (noValidPoint)
-                processingPointList.RemoveAt(randomProcessingListIndex);
+            return true;
+        }
+
+        private Point2D PickStartPoint()
+		{
+            return StartPointPickMode switch
+			{
+				StartPointPickMode.Random => new Point2D
+                {
+                    x = (float)(AreaWidth * random.NextDouble()),
+                    y = (float)(AreaHeight * random.NextDouble())
+                },
+				StartPointPickMode.Center => new Point2D
+                {
+                    x = AreaWidth * 0.5f,
+                    y = AreaHeight * 0.5f
+                },
+				StartPointPickMode.TopLeft => new Point2D
+                {
+                    x = 0.0f,
+                    y = AreaHeight
+                },
+				StartPointPickMode.TopRight => new Point2D
+                {
+                    x = AreaWidth,
+                    y = AreaHeight
+                },
+				StartPointPickMode.BottomLeft => new Point2D
+                {
+                    x = 0.0f,
+                    y = 0.0f
+                },
+				StartPointPickMode.BottomRight => new Point2D
+                {
+                    x = AreaWidth,
+                    y = 0.0f
+                },
+				StartPointPickMode.Custom => CustomStartPoint,
+				_ => new Point2D()//never happen
+			};
 		}
 
-        points = outputPointList.ToArray();
+        public void SetSeed(int seed)
+        {
+            this.seed = seed;
+            random = new Random(seed);
+        }
+        public void GeneratePoints(ref Point2D[] points)
+        {
+            //Determine Grid Size
+            cellSize = Radius / Sqrt2;
+            gridWidth = CeilToInt(AreaWidth / cellSize);
+            gridHeight = CeilToInt(AreaHeight / cellSize);
 
-        grid = null;
-        outputPointList.Clear();
-        processingPointList.Clear();
-	}
+            //Initialize Grid and List
+            grid = new int[gridWidth, gridHeight];
+            grid.Fill(-1);
+
+            outputPointList.Clear();
+            processingPointList.Clear();
+
+            //Pick a Starting Point
+            var startPoint = PickStartPoint();
+
+            //Add the start point to the output list, processing list and grid
+            outputPointList.Add(startPoint);
+            processingPointList.Add(0);
+            grid[FloorToInt(startPoint.x / cellSize), FloorToInt(startPoint.y / cellSize)] = 0;
+
+            //Process
+            while (processingPointList.Count > 0)
+            {
+                //Choose a random index from the processing list
+                var randomProcessingListIndex = random.Next(processingPointList.Count);
+                var outputPointIndex = processingPointList[randomProcessingListIndex];
+                var point = outputPointList[outputPointIndex];
+
+                //Generate sampleLimitBeforeRejection Points
+                var noValidPoint = true;
+                for (int i = 0; i < SampleLimitBeforeRejection; i++)
+                {
+                    //Generate random angle and distance
+                    var angle = Pi2 * (float)random.NextDouble();
+                    var distance = Lerp(Radius, 2 * Radius, (float)random.NextDouble());
+
+                    //Create new Point
+                    var newPoint = new Point2D
+                    {
+                        x = point.x + distance * (float)Math.Cos(angle),
+                        y = point.y + distance * (float)Math.Sin(angle)
+                    };
+
+                    //Check if it is within distance r of existing points
+                    if (IsValidPointPosition(newPoint))
+                    {
+                        //Add the new point to the output list, processing list and grid
+                        var gridX = FloorToInt(newPoint.x / cellSize);
+                        var gridY = FloorToInt(newPoint.y / cellSize);
+
+                        outputPointList.Add(newPoint);
+                        grid[gridX, gridY] = outputPointList.Count - 1;
+                        processingPointList.Add(outputPointList.Count - 1);
+
+                        noValidPoint = false;
+                    }
+                }
+
+                //If no such point is found, remove point from the processing list
+                if (noValidPoint)
+                    processingPointList.RemoveAt(randomProcessingListIndex);
+            }
+
+            points = outputPointList.ToArray();
+
+            grid = null;
+            outputPointList.Clear();
+            processingPointList.Clear();
+        }
+    }
 }
