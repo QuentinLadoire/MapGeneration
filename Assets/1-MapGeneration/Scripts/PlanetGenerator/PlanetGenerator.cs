5ee4d2dc3af15a339c28d14dca4a072fb30b09d5
Quentin Ladoire <quentinladoire@gmail.com>
2021-12-27 01:06:21 +0100

    Changing the process of creating the planet mesh

--- Assets/1-MapGeneration/Scripts/PlanetGenerator/PlanetGenerator.cs
+++ Assets/1-MapGeneration/Scripts/PlanetGenerator/PlanetGenerator.cs
@@ -4,6 +4,100 @@ using UnityEngine;
 using Geometry;
 using Geometry.DataStructure;
 
+using GeometryUtility = Geometry.GeometryUtility;
+
+public class TectonicPlate
+{
+	public int[] cells = null;
+}
+
+public class Planet
+{
+	private Mesh mesh = null;
+	private bool[] freeCells = null;
+	private MeshData meshData = null;
+	private TectonicPlate[] tectonicPlates = null;
+	private HalfEdgeData polygonHalfEdgeData = null;
+
+	public Mesh Mesh => mesh;
+
+	private void ComputeFace(Face face)
+	{
+		var polygon = new Vector3[face.edgeCount];
+
+		var faceEdgeCount = face.edgeCount;
+		var verticesCount = meshData.VerticesCount;
+
+		var halfEdge = face.First;
+		for (int i = 0; i < faceEdgeCount; i++)
+		{
+			polygon[i] = halfEdge.Vertex;
+
+			var firstIndex = verticesCount + i;
+			var secondIndex = (i != faceEdgeCount - 1) ? (verticesCount + i + 1) : verticesCount;
+			var thirdIndex = verticesCount + faceEdgeCount;
+
+			meshData.AddTriangle(firstIndex, secondIndex, thirdIndex);
+			meshData.AddVertex(halfEdge.Vertex);
+
+			halfEdge = halfEdge.Next;
+		}
+
+		meshData.AddVertex(GeometryUtility.CalculateBarycenter(polygon));
+	}
+	public void ComputeGeometry()
+	{
+		if (meshData == null)
+			meshData = new MeshData();
+
+		var faces = polygonHalfEdgeData.faces;
+		for (int i = 0; i < faces.Length; i++)
+			ComputeFace(faces[i]);
+	}
+
+	private bool ComputeTectonicPlate(int plateIndex, int[] processingCellIndexes)
+	{
+		return false;
+	}
+	public void ComputeTectonicPlates(int count = 5)
+	{
+		freeCells.Fill(true);
+
+		var faceCount = polygonHalfEdgeData.faces.Length;
+
+		var processingCellIndexes = new int[count];
+		for (int i = 0; i < count; i++)
+			processingCellIndexes[i] = Random.Range(0, faceCount);
+
+		tectonicPlates = new TectonicPlate[count];
+		while (faceCount > 0)
+		{
+			for (int i = 0; i < count; i++)
+			{
+				if (ComputeTectonicPlate(i, processingCellIndexes))
+					faceCount--;
+			}
+		}
+	}
+
+	public void ComputeMesh()
+	{
+		if (mesh == null)
+			mesh = new Mesh();
+
+		mesh.SetVertices(meshData.Vertices);
+		mesh.SetTriangles(meshData.Triangles, 0);
+
+		mesh.RecalculateNormals();
+	}
+
+	public Planet(HalfEdgeData polygonHalfEdgeData)
+	{
+		this.polygonHalfEdgeData = polygonHalfEdgeData;
+		this.freeCells = new bool[this.polygonHalfEdgeData.faces.Length];
+	}
+}
+
 [ExecuteAlways]
 public class PlanetGenerator : MonoBehaviour
 {
@@ -11,6 +105,7 @@ public class PlanetGenerator : MonoBehaviour
 	[SerializeField] private Material planetMaterial = null;
 	[SerializeField] private float planetRadius = 1.0f;
 	[SerializeField] private int planetRefiningStep = 3;
+	[SerializeField] private int planetTectonicPlateCount = 5;
 
 	[Header("Debug Settings")]
 	[SerializeField] private Mesh cubeMesh = null;
@@ -23,9 +118,8 @@ public class PlanetGenerator : MonoBehaviour
 	[SerializeField] private bool renderTriangleData = false;
 	[SerializeField] private bool renderPolygonData = false;
 
-	private Mesh mesh = null;
-	private MeshData icoSphereMeshData = null;
-	private MeshData goldbergPolyhedronMeshData = null;
+	[SerializeField] private Mesh mesh = null;
+	private Planet planet = null;
 	private DualHalfEdgeData dualMeshData = null;
 
 	private void DrawPlanet()
@@ -88,17 +182,13 @@ public class PlanetGenerator : MonoBehaviour
 
 	private void GenerateShape()
 	{
-		icoSphereMeshData = MeshGenerator.CreateIcoSphere(planetRadius, planetRefiningStep);
-		DataStructureBuilder.CreateDualMeshData(icoSphereMeshData, out dualMeshData);
+		DataStructureBuilder.CreateDualMeshData(MeshGenerator.CreateIcoSphere(planetRadius, planetRefiningStep), out dualMeshData);
 
-		goldbergPolyhedronMeshData = MeshGenerator.CreateMeshFromHalfEdgePolygon(dualMeshData.polygonData);
+		planet = new Planet(dualMeshData.polygonData);
+		planet.ComputeGeometry();
+		planet.ComputeMesh();
 
-		mesh = new Mesh
-		{
-			vertices = goldbergPolyhedronMeshData.vertices,
-			triangles = goldbergPolyhedronMeshData.triangles
-		};
-		mesh.RecalculateNormals();
+		mesh = planet.Mesh;
 	}
 	public void Generate()
 	{
